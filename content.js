3b9a3ad9b658f8eac74077ecaef118e0f41422ec
David Dales <david.c.dales@gmail.com>
2020-09-13 00:15:00 -0400

    added vanilla js event listener

--- content.js
+++ content.js
@@ -1,21 +1,32 @@
-var mouseX = {
-  getScreenSize() {
-    this.screenWidth = $("body").width();
-    this.screenHeight = $("body").height();
-  },
-  getWindowSize() {},
-  init() {
-    console.log("init...2");
+window.onload = function () {
+  var mouseX = {
+    getScreenSize() {
+      this.screenWidth = this.body.height;
+      this.screenHeight = this.body.height;
+    },
+    getWindowSize() {},
+    addEventListeners() {
+      console.log("this.body");
+      console.log(this.body);
+      document.addEventListener("keydown", function (e) {
+        console.log(e.keyCode);
+      });
+    },
+    init() {
+      console.log("init...2");
 
-    this.body = document.querySelector("body");
+      this.body = document.querySelector("body");
 
-    var newDiv = document.createElement("div");
-    newDiv.style.cssText =
-      "width:10px;height:10px;background:red;position:absolute;left:0;top:0;";
-    newDiv.innerHTML = "hi";
+      var newDiv = document.createElement("div");
+      newDiv.style.cssText =
+        "width:10px;height:10px;background:red;position:absolute;left:0;top:0;";
+      newDiv.innerHTML = "hi";
 
-    this.body.appendChild(newDiv);
-  },
+      this.body.appendChild(newDiv);
+
+      this.addEventListeners();
+    },
+  };
+
+  mouseX.init();
 };
-
-mouseX.init();
