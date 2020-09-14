329e6043d5b519dc4b7c9fe92b97f2ed50672829
David Dales <david.c.dales@gmail.com>
2020-09-14 06:38:28 -0400

    altered start position

--- content.js
+++ content.js
@@ -10,8 +10,8 @@ window.onload = function () {
     clientSpeedY: 0,
     speedIncrements: 0.1,
 
-    clientX: 0,
-    clientY: 0,
+    clientX: 25,
+    clientY: 50,
 
     newDiv: document.createElement("div"),
     body: document.querySelector("body"),
@@ -248,7 +248,7 @@ window.onload = function () {
         height: 50px; 
 
         position:absolute;
-        left:0;
+        left:-25;
         top:0;
         pointer-events:none;
         background: url(${chrome.extension.getURL("ufo.gif")});
